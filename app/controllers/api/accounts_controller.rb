3c29dad55a6fc1f6fa71e6d8f6c1b73cd4c366b5
Roberto Rivas <roberto.rivas.dev@gmail.com>
2022-01-10 22:49:13 -0400

    🚑️ Fix public endpoint for account user

--- app/controllers/api/accounts_controller.rb
+++ app/controllers/api/accounts_controller.rb
@@ -1,5 +1,6 @@
 class Api::AccountsController < ApplicationController
   skip_before_action :verify_authenticity_token
+  skip_before_action :authenticate_user!
 
   def show
     account = Account.find_by(id: params[:id])
